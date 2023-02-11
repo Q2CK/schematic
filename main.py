d490b0d1d6ae11a42fc134e75b968b4e66598b29
Q2CK <109765887+Q2CK@users.noreply.github.com>
2023-02-11 16:44:35 +0100

    f

--- main.py
+++ main.py
@@ -20,57 +20,49 @@ def rearrange(lines: list[str]):
     return output
 
 
+def place_line(schem, line_nr, line, diag_offset, z_offset, repeater):
+
+    for bit, char in enumerate(line):
+        if char == "0":
+            schem.setBlock((bit + 2 + diag_offset + (2 * line_nr), bit - (2 * line_nr) - diag_offset, 2 + z_offset),
+                           "minecraft:white_stained_glass")
+        elif char == "1":
+            schem.setBlock((bit + 2 + diag_offset + (2 * line_nr), bit - (2 * line_nr) - diag_offset, 2 + z_offset),
+                           repeater)
+
+
 def main():
     schem = mcschematic.MCSchematic()
 
-    bin_file = rearrange(open("bin.txt", "r").readlines())
+    bin_raw = open("bin.txt", "r").readlines()
+    bin_rearranged = rearrange(bin_raw)
 
-    for line in bin_file:
+    for line in bin_rearranged:
         print(line)
 
-    for line_nr, line in enumerate(bin_file[0:8]):
+    for line_nr, line in enumerate(bin_rearranged[0:8]):
+        place_line(schem, line_nr, line, 0, 0, "minecraft:repeater[facing=north]")
 
-        for bit, char in enumerate(line):
-            if char == "0":
-                schem.setBlock((bit + 2 + (2 * line_nr), bit - (2 * line_nr), 2), "minecraft:white_stained_glass")
-            elif char == "1":
-                schem.setBlock((bit + 2 + (2 * line_nr), bit - (2 * line_nr), 2), "minecraft:repeater[facing=north]")
+    for line_nr, line in enumerate(bin_rearranged[8:15]):
+        place_line(schem, line_nr, line, 1, 0, "minecraft:repeater[facing=north]")
 
-    for line_nr, line in enumerate(bin_file[8:15]):
+    for line_nr, line in enumerate(bin_rearranged[16:24]):
+        place_line(schem, line_nr, line, 0, 3, "minecraft:repeater[facing=south]")
 
-        for bit, char in enumerate(line):
-            if char == "0":
-                schem.setBlock((bit + 3 + (2 * line_nr), bit - (2 * line_nr) - 1, 2), "minecraft:white_stained_glass")
-            elif char == "1":
-                schem.setBlock((bit + 3 + (2 * line_nr), bit - (2 * line_nr) - 1, 2), "minecraft:repeater[facing=north]")
+    for line_nr, line in enumerate(bin_rearranged[24:31]):
+        place_line(schem, line_nr, line, 1, 3, "minecraft:repeater[facing=south]")
 
-    for line_nr, line in enumerate(bin_file[16:24]):
+    for line_nr, line in enumerate(bin_rearranged[32:40]):
+        place_line(schem, line_nr, line, 1, 8, "minecraft:repeater[facing=north]")
 
-        for bit, char in enumerate(line):
-            if char == "0":
-                schem.setBlock((bit + 2 + (2 * line_nr), bit - (2 * line_nr), 5), "minecraft:white_stained_glass")
-            elif char == "1":
-                schem.setBlock((bit + 2 + (2 * line_nr), bit - (2 * line_nr), 5), "minecraft:repeater[facing=south]")
+    for line_nr, line in enumerate(bin_rearranged[40:47]):
+        place_line(schem, line_nr, line, 2, 8, "minecraft:repeater[facing=north]")
 
-    for line_nr, line in enumerate(bin_file[24:31]):
+    for line_nr, line in enumerate(bin_rearranged[48:56]):
+        place_line(schem, line_nr, line, 1, 11, "minecraft:repeater[facing=south]")
 
-        for bit, char in enumerate(line):
-            if char == "0":
-                schem.setBlock((bit + 3 + (2 * line_nr), bit - (2 * line_nr) - 1, 5), "minecraft:white_stained_glass")
-            elif char == "1":
-                schem.setBlock((bit + 3 + (2 * line_nr), bit - (2 * line_nr) - 1, 5), "minecraft:repeater[facing=south]")
-
-    for line_nr, line in enumerate(bin_file[32:40]):
-        pass
-
-    for line_nr, line in enumerate(bin_file[40:47]):
-        pass
-
-    for line_nr, line in enumerate(bin_file[48:56]):
-        pass
-
-    for line_nr, line in enumerate(bin_file[56:63]):
-        pass
+    for line_nr, line in enumerate(bin_rearranged[56:63]):
+        place_line(schem, line_nr, line, 2, 11, "minecraft:repeater[facing=south]")
 
     schem.save("C:/Users/Oem/PycharmProjects/schematic", "ms", mcschematic.Version.JE_1_16_5)
 
